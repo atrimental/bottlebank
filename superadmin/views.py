a8c2f77ebd815429faa31c0e032e77de694a32ab
Emily Shackleton <emily@trillian.ksa>
2018-12-23 16:38:01 +0000

    Add jinja templates

--- superadmin/views.py
+++ superadmin/views.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
-import models.conn
+from models.game import Game
+import templates
 
 bp = Blueprint('superadmin', __name__)
 
 @bp.route('/')
 def super_admin_home():
-    return "Super"
+    page = templates.env.get_template('superadmin/index.html')
+    return page.render(games=Game.get_all_games())
